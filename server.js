682984bd9bd2f538c6553770204bdad0ff3a0466
GSaiCharanTeja <gavidisaicharanteja@gmail.com>
2026-01-20 17:26:19 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -101,6 +101,9 @@ app.get("/api/like/:image", async (req, res) => {
 });
 
 /* ================= START SERVER ================= */
-app.listen(5000, () => {
-    console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
